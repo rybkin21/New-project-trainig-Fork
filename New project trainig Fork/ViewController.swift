f35e31000e7a1cbb221828d92a27248c1e6d7574
ivanrybkin <rybkin2517@mail.ru>
2023-05-20 18:30:35 +0300

    Initial Commit

---
+++ New project trainig Fork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  New project trainig Fork
+//
+//  Created by Ivan Rybkin on 20.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
